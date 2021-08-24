22e3f9bdf3c594a1020f1dfb2635e326512044f5
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-23 20:47:38 -0500

    agregando opciones custom a artist

--- src/songs/models/Artist.js
+++ src/songs/models/Artist.js
@@ -1,26 +1,32 @@
 const { Schema, model } = require("mongoose");
 
-const artistSchema = new Schema({
-  artist: {
-    type: String,
-    required: true,
-  },
-  verify: {
-    type: Boolean,
-    default: false,
-  },
-  albums: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Album",
+const artistSchema = new Schema(
+  {
+    artist: {
+      type: String,
+      required: true,
     },
-  ],
-  songs: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Song",
+    verify: {
+      type: Boolean,
+      default: false,
     },
-  ],
-});
+    albums: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Album",
+      },
+    ],
+    songs: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Song",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
 module.exports = model("Artist", artistSchema);
