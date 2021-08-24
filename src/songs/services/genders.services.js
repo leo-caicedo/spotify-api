39c2bba3aee3026fc5ad26ceaa51c83d7866a165
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-24 13:17:08 -0500

    servicios de genders

---
+++ src/songs/services/genders.services.js
@@ -0,0 +1,71 @@
+// models
+const Gender = require("../models/Gender");
+
+class GenderServices {
+  // list genders
+  async getGenders(req, res, next) {
+    try {
+      const genders = await Gender.find({}).populate("songs", {
+        _id: 0,
+        song: 1,
+      });
+      res.json(genders);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  // get gender
+  async getGender(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      const gender = await Gender.findById(id).populate("songs");
+      res.json(gender);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  // create gender
+  async createGender(req, res, next) {
+    const { body: gender } = req;
+
+    try {
+      const genderCreated = new Gender(gender);
+      await genderCreated.save();
+      res.status(201).json(genderCreated);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  // update gender
+  async updateGender(req, res, next) {
+    const { id } = req.params;
+    const { body: gender } = req;
+
+    try {
+      const genderUpdated = await Gender.findByIdAndUpdate(id, gender, {
+        new: true,
+      });
+      res.json(genderUpdated);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  // delete gender
+  async deleteGender(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      await Gender.findByIdAndDelete(id);
+      res.status(204).end();
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = GenderServices;
