ba350cbd7464dfa29a939399b37a4b828c27143c
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-24 12:07:57 -0500

    getArtist

--- src/songs/services/artist.services.js
+++ src/songs/services/artist.services.js
@@ -6,10 +6,25 @@ class ArtistServices {
   async getArtists(req, res, next) {
     try {
       const artists = await Artist.find({})
-        .populate("albums", { __id: 0, album: 1 })
-        .populate("songs", { __id: 0, song: 1 })
+        .populate("albums", { _id: 0, album: 1 })
+        .populate("songs", { _id: 0, song: 1 })
         .exec();
       res.json(artists);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  // get artist
+  async getArtist(req, res) {
+    const { id } = req.params;
+
+    try {
+      const artist = await Artist.findById(id)
+        .populate("album")
+        .populate("songs")
+        .exec();
+      res.json(artist);
     } catch (err) {
       next(err);
     }
