bdc152cc2176be2827e7ea41e0b8022f6045dde5
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-23 21:26:51 -0500

    artist dto

---
+++ src/songs/dtos/artist.dto.js
@@ -0,0 +1,14 @@
+const { body } = require("express-validator");
+
+const artistDto = [
+  // artist
+  body("artist")
+    .exists({ checkFalsy: false })
+    .withMessage("Artist name is required")
+    .isLength({ max: 30 })
+    .withMessage("The artist name must have a maximum of 30 characters"),
+  // verify
+  body("verify").isBoolean(),
+];
+
+module.exports = artistDto;
